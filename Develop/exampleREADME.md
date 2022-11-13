7c8786cb9adf64b41c717c15400ee8c6ba35f484
jjohnson673 <jkjohnson673@gmail.com>
2022-11-13 18:09:55 -0500

    added in text for generatemarkedown file

---
+++ Develop/exampleREADME.md
@@ -0,0 +1,42 @@
+
+  # REAMDE Project
+
+  ## Description
+  
+  where we create a readme generator
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Installation
+
+  n/a
+
+  ## Usage
+
+  answer questiosn
+
+  ## Contributing
+
+  n/a
+
+  ## Tests
+
+  n/a
+
+  ## License
+
+  This project is licensed under the MIT license.
+
+
+  ## Questions?
+
+  For any questions, you can contact me via email at julia 
+  or you can connect with me on <a href=" https://github.com/jkfldjiso" target="_blank">Github</a> 
+ 
--- Develop/index.js
+++ Develop/index.js
@@ -4,14 +4,88 @@ const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+
+//const generateMD = ({title, description, installation, usage, contributing, license, test, github, email}) =>
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'title',
+    },
+
+    {
+      type: 'input',
+      message: 'Project description',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Enter any steps required to install your projects. If no steps are needed, input "N/A".',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How do you use your app?',
+      name: 'usage',
+    },
+
+    {
+      type: 'input',
+      message: 'How can others contribute to your project?',
+      name: 'contributing',
+
+    },
+    {
+      type: 'input',
+      message: 'If necessary, provide tests for your app and how to test it. If none apply, input "N/A"',
+      name: 'tests',
+
+    },
+
+    {
+      type: 'list',
+      message: 'License?',
+      name: 'license',
+      choices: ['MIT', 'Mozilla Public License', 'Apache', 'GPLv3', 'BSD 3-Clause','None']
+
+    },
+    {
+     type: 'input',
+     message: 'What is your GitHub username?',
+     name: 'github',
+  
+    },
+    {
+     type: 'input',
+     message: 'Enter your preferred email for your contact information',
+     name: 'email',
+  
+    },
+  ])
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then((data) => {
+    const readmeContent = generateMarkdown(data);
+
+    fs.writeFile('exampleREADME.md', readmeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created your README documentation!')
+    );
+  });
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+      .then(function(data) {
+          writeToFile('./develop/README.md', markdownGenerator(data));
+          console.log(data)
+          
+  })    
+}
 
 // Function call to initialize app
-init();
+// init();
+ 
